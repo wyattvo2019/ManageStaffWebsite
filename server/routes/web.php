185658c7ae9cea52da93ee7affc8c3a053e5da8b
Wyatt VO <votranquy96@gmail.com>
2024-04-17 17:06:37 +0700

    TB-20240417: Create Controller

--- server/routes/web.php
+++ server/routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\DB;
+// use Models\Member;
+use App\Models\Candidate;
+use App\Models\InterviewerComment;
+use App\Models\Interview;
+use App\Models\Interviewer;
+use App\Models\Configuration;
+use App\Http\Controllers\MemberController;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('test', function () {
+    // return DB::table('member')->get();
+    $elm = Configuration::find(9);
+    return $elm->getCandidateHasSkill;
+});
+Route::get('xuly', [MemberController::class,'xxx']);
+Route::resource('xuly', [MemberController::class,'xxx']);
+
