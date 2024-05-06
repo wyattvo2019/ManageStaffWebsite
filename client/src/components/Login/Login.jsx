842d3c9c903626955a398cae3289b90e9f572f3f
Wyatt VO <votranquy96@gmail.com>
2024-05-06 17:16:53 +0700

    TB-20240506: Handle login form

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,28 +1,78 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import { FormField, Button, Form, Container, Card, Segment, Input, Image} from 'semantic-ui-react'
+import { FormField, Button, Form, Container, Card, Segment, Input, Image, Loader} from 'semantic-ui-react'
 import "./Login.scss";
 class Login extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      isLoading: false,
+      username:'',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+  }
+  handleChange(event){
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
+    this.setState({
+      [name]: value
+    });
+    console.log(this.state.username);
+
+  }
+  handleSubmit(){
+    let {username, password} = this.state;
+    if(username !== '' && password !== ''){
+      this.setState({isLoading: true});
+      console.log(this.state);
+    }
+  }
+
   render(){
+    let {isLoading, username, password} = this.state;
     return (
       <Container>
         {/* <Col> */}
         <Card className='form-login'>
           <Segment color='red'>
+            <Loader active={isLoading} size="massive" />
             <Image id="logo" src="/images/logo.png" alt=""/>
             <Form>
-              <FormField label="Username" name="username" control={Input} placedhoder="Username" required >
+              <FormField 
+              label="Username" 
+              name="username" 
+              control={Input} 
+              placedhoder="Username" 
+              required 
+              value={username}
+              onChange={this.handleChange}>
                 {/* <label>Username</label> */}
                 {/* <input placeholder='First Name' /> */}
               </FormField>
-              <FormField label="Password" name="password" control={Input} placedhoder="password" required >
+              <FormField 
+              abel="Password" 
+              name="password" 
+              control={Input} 
+              placedhoder="password" 
+              required 
+              value={password}
+              onChange={this.handleChange}
+              >
                 {/* <label>Password</label>
                 <input placeholder='Last Name' /> */}
               </FormField>
               {/* <FormField>
                 <Checkbox label='I agree to the Terms and Conditions' />
               </FormField> */}
-              <Button fluid type='submit' color="blue">Submit</Button>
+              <Button fluid 
+              type='submit' 
+              color="blue"
+              onClick={this.handleSubmit}
+              >Submit</Button>
             </Form>
           </Segment>
         </Card>
